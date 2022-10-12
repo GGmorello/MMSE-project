f2575f11f64fd68fb9545f645675ba99b569a1e8
perttu <perttu_1995@hotmail.com>
2022-10-12 21:00:15 +0200

    minor model change

--- frontend/src/model/event.ts
+++ frontend/src/model/event.ts
@@ -5,8 +5,8 @@ export interface EventRequestItem {
 
 export interface EventBase {
     clientId: string;
-    startDate: string; // ISO String: 2020-10-10T10:00:00Z
-    endDate: string; // ISO String: 2020-10-10T10:00:00Z
+    startDate: string; // YYYY-MM-DD
+    endDate: string; // YYYY-MM-DD
     eventRequestItems: EventRequestItem[];
 }
 
