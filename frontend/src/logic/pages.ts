2d67072190330f8609a7a695c47bba03e10189fb
perttuj <perttu_1995@hotmail.com>
2022-10-19 02:40:51 +0200

    MMSE-11: Submit Hiring Request (#11)
    * MMSE-45: Submit Hiring Request UI
    
    * MMSE-54: Submit Hiring Request API/DB
    
    * MMSE-45: Submit Hiring Request UI integration

--- frontend/src/logic/pages.ts
+++ frontend/src/logic/pages.ts
@@ -9,11 +9,11 @@ export function getRolePages(role: Role): Page[] {
         case Role.FINANCIAL_MANAGER:
             return [Page.BROWSE_EVENT_REQUEST, Page.BROWSE_FINANCIAL_REQUEST];
         case Role.PRODUCTION_MANAGER:
-            return [Page.BROWSE_FINANCIAL_REQUEST];
+            return [Page.BROWSE_FINANCIAL_REQUEST, Page.SUBMIT_HIRING_REQUEST];
         case Role.SENIOR_CUSTOMER_SERVICE_OFFICER:
             return [Page.BROWSE_EVENT_REQUEST];
         case Role.SERVICE_MANAGER:
-            return [Page.BROWSE_FINANCIAL_REQUEST];
+            return [Page.BROWSE_FINANCIAL_REQUEST, Page.SUBMIT_HIRING_REQUEST];
         default:
             console.warn(
                 "Unexpected role received, cannot calculate routes.",
@@ -31,6 +31,8 @@ export function getPageLabel(page: Page): string {
             return "Browse financial requests";
         case Page.SUBMIT_EVENT_REQUEST:
             return "Submit event request";
+        case Page.SUBMIT_HIRING_REQUEST:
+            return "Submit hiring request";
         default:
             console.warn(
                 "Unexpected page received, cannot calculate route label:",
--- frontend/src/store/user/userSlice.ts
+++ frontend/src/store/user/userSlice.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import WebService from "components/common/WebService";
 import { ResponseType } from "model/api";
-import { User, Response, LoadingState, FinancialRequest } from "model";
+import { User, Response, LoadingState, Role, FinancialRequest } from "model";
 import { RootState } from "store/store";
 
 interface LoginRequest {
@@ -95,6 +95,37 @@ export const logoutUser = createAsyncThunk(
     },
 );
 
+interface HiringRequest {
+    requestedRole: Role;
+    comment: string;
+}
+
+export const submitHiringRequest = createAsyncThunk(
+    "user/hire",
+    async ({ requestedRole, comment }: HiringRequest, thunkAPI) => {
+        const state: RootState = thunkAPI.getState() as RootState;
+        const user: User | null = state.user.userData;
+        if (user === null) {
+            console.warn("user data is null - cannot save event");
+            return thunkAPI.rejectWithValue("user is null");
+        }
+        const service: WebService = new WebService(user.access_token);
+        const response: Response<HiringRequest> = await service.submitHiringRequest(user.role, requestedRole, comment);
+        switch (response.type) {
+            case ResponseType.SUCCESSFUL:
+                return response.data;
+            case ResponseType.ERROR:
+                return thunkAPI.rejectWithValue(undefined);
+            default:
+                console.log(
+                    "unexpected return type from login request: ",
+                    response,
+                );
+                return thunkAPI.rejectWithValue(undefined);
+        }
+    },
+);
+
 export interface UserState {
     userData: User | null;
     financialRequests: FinancialRequest[];
@@ -124,6 +155,15 @@ export const userSlice = createSlice({
                 state.userData = null;
                 state.financialRequests = [];
                 state.loading = LoadingState.IDLE;
+            })
+            .addCase(submitHiringRequest.fulfilled, (state: UserState, action) => {
+                state.loading = LoadingState.SUCCEEDED;
+            })
+            .addCase(submitHiringRequest.pending, (state: UserState) => {
+                state.loading = LoadingState.PENDING;
+            })
+            .addCase(submitHiringRequest.rejected, (state: UserState) => {
+                state.loading = LoadingState.FAILED;
             })
             .addCase(fetchFinancialRequests.fulfilled, (state: UserState) => {
                 state.loading = LoadingState.SUCCEEDED;
